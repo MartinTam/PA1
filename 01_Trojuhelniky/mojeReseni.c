fcd4be0333c4a3fa170c7b0f736567a7b29716e5
MartinTam <hongvtam@protonmail.com>
2019-10-20 16:08:41 +0200

    (SUS vs USU) + reverse
    be ostreni porovnavani doublu

--- 01_Trojuhelniky/mojeReseni.c
+++ 01_Trojuhelniky/mojeReseni.c
@@ -648,9 +648,9 @@ if ( tr2_va1 <= 0 || tr2_va2 <= 0 || tr1_va2 <= 0 ){
 
               // Prevod uhlu na radiany:
 
-                    double alpha = tr2_va1 * (180 / M_PI);
-                    double beta = tr2_va3 * (180 / M_PI);
-                    double gama = j * (180 / M_PI);
+                    double alpha = tr2_va1 * (M_PI / 180);
+                    double beta = tr2_va3 * (M_PI / 180);
+                    double gama = j * (M_PI / 180);
 
 
               // Dopocitavani stran u triangle2:
@@ -768,9 +768,9 @@ if ( tr2_va1 <= 0 || tr2_va2 <= 0 || tr1_va2 <= 0 ){
 
           // Prevod uhlu na radiany:
 
-                double alpha = tr1_va1 * (180 / M_PI);
-                double beta = tr1_va3 * (180 / M_PI);
-                double gama = j * (180 / M_PI);
+                double alpha = tr1_va1 * (M_PI / 180);
+                double beta = tr1_va3 * (M_PI / 180);
+                double gama = j * (M_PI / 180);
 
 
           // Dopocitavani stran u triangle1:
@@ -879,6 +879,292 @@ if ( tr2_va1 <= 0 || tr2_va2 <= 0 || tr1_va2 <= 0 ){
 
 
 
+
+
+
+
+
+
+
+
+
+// SUS vs USU:
+
+
+          if ( tr1_ch1 == 'S' && tr1_ch2 == 'U' && tr1_ch3 == 'S' &&  tr2_ch1 == 'U' && tr2_ch2 == 'S' && tr2_ch3 == 'U' ){
+
+                  // Prevod uhlu na radiany:
+
+                      // Triangle1:
+
+                          double uhel_tr1 = tr1_va2 * ( M_PI / 180 );
+
+                      // Triangle2:
+
+                          double tretiUhel = 180 - (tr2_va1 + tr2_va3);       // Vypocet 3.uhlu v triangle2
+
+                          double gama = tretiUhel * ( M_PI / 180 );
+                          double alpha = tr2_va1 * ( M_PI / 180 );
+                          double beta = tr2_va3 * ( M_PI / 180 );
+
+
+
+                  // Vypocet 3.strany v triangle1:
+
+                      double c = sqrt( (tr1_va1 * tr1_va1) + ( tr1_va3 * tr1_va3 ) - ( 2 * tr1_va1 * tr1_va3 * cos(uhel_tr1) ) );
+
+                  // Vypocet zbylych 2 stran v triangle2:
+
+                      double y = tr2_va2 * ( sin(alpha) / sin(gama) );
+                      double z = tr2_va2 * (sin(beta) / sin(gama));
+                      double x = tr2_va2;
+
+
+
+
+
+                      // Serazeni stran:
+
+                                                        // Serazeni delek stran tr1:
+
+
+                                                                if ( tr1_va1 > tr1_va3 ){
+                                                                  double tmp = tr1_va3;
+                                                                  tr1_va3 = tr1_va1;
+                                                                  tr1_va1 = tmp;
+                                                                };
+
+                                                                if ( tr1_va3 > c ){
+                                                                  double tmp = c;
+                                                                  c = tr1_va3;
+                                                                  tr1_va3 = tmp;
+                                                                };
+
+                                                                if ( tr1_va1 > tr1_va3 ){
+                                                                  double tmp = tr1_va3;
+                                                                  tr1_va3 = tr1_va1;
+                                                                  tr1_va1 = tmp;
+                                                                };
+
+
+
+
+
+
+
+                                                        // Serazeni delek stran tr2:
+
+
+                                                                if ( z > x ){
+                                                                  double tmp = x;
+                                                                  x = z;
+                                                                  z = tmp;
+                                                                };
+
+                                                                if ( x > y ){
+                                                                  double tmp = y;
+                                                                  y = x;
+                                                                  x = tmp;
+                                                                };
+
+                                                                if ( z > x ){
+                                                                  double tmp = x;
+                                                                  x = z;
+                                                                  z = tmp;
+                                                                };
+
+
+
+
+
+
+
+
+                  // Shodnost:
+
+
+                      if ( tr1_va1 == z && tr1_va3 == x && c == y ){
+                        shodne = 1;
+                      }
+                      else{
+                        shodne = 0;
+                      };
+
+
+                  // Podobnost:
+
+
+                      if ( ( (tr1_va1 / z) == (tr1_va3 / x) ) && ( (tr1_va3 / x) == (c / y) ) && ( (tr1_va1 / z) == (c / y) ) ){
+                        podobne = 1;
+                      }
+                      else{
+                        podobne = 0;
+                      };
+
+
+
+                  // Vypis:
+
+
+
+
+
+                      if ( shodne == 1 && podobne == 1 ){
+                        printf("Trojuhelniky jsou shodne.\n");
+                        return 0;
+                      }
+                      else if ( shodne == 1 && podobne == 0 ){
+                        printf("Trojuhelniky jsou shodne.\n");
+                        return 0;
+                      }
+                      else if ( shodne == 0 && podobne == 1 ){
+                        printf("Trojuhelniky nejsou shodne, ale jsou podobne.\n");
+                        return 0;
+                      }
+                      else if ( shodne == 0 && podobne == 0 ){
+                        printf("Trojuhelniky nejsou shodne ani podobne.\n");
+                        return 0;
+                      };
+
+
+
+          };
+
+
+
+
+
+// USU vs SUS
+
+
+        if ( tr1_ch1 == 'U' && tr1_ch2 == 'S' && tr1_ch3 == 'U' &&  tr2_ch1 == 'S' && tr2_ch2 == 'U' && tr2_ch3 == 'S' ){
+
+        // Prevod uhlu na radiany:
+
+            // Triangle2:
+
+                double uhel_tr2 = tr2_va2 * ( M_PI / 180 );
+
+            // Triangle1:
+
+                double tretiUhel = 180 - (tr1_va1 + tr1_va3);       // Vypocet 3.uhlu v triangle1
+
+                double gama = tretiUhel * ( M_PI / 180 );
+                double alpha = tr1_va1 * ( M_PI / 180 );
+                double beta = tr1_va3 * ( M_PI / 180 );
+
+
+            // Vypocet 3.strany v triangle2:
+
+                double c = sqrt( (tr2_va1 * tr2_va1) + ( tr2_va3 * tr2_va3 ) - ( 2 * tr2_va1 * tr2_va3 * cos(uhel_tr2) ) );
+
+            // Vypocet zbylych 2 stran v triangle1:
+
+                double y = tr1_va2 * ( sin(alpha) / sin(gama) );
+                double z = tr1_va2 * (sin(beta) / sin(gama));
+                double x = tr1_va2;
+
+
+                // Serazeni stran:
+
+                                                  // Serazeni delek stran tr2:
+
+
+                                                          if ( tr2_va1 > tr2_va3 ){
+                                                            double tmp = tr2_va3;
+                                                            tr2_va3 = tr2_va1;
+                                                            tr2_va1 = tmp;
+                                                          };
+
+                                                          if ( tr2_va3 > c ){
+                                                            double tmp = c;
+                                                            c = tr2_va3;
+                                                            tr2_va3 = tmp;
+                                                          };
+
+                                                          if ( tr2_va1 > tr2_va3 ){
+                                                            double tmp = tr2_va3;
+                                                            tr2_va3 = tr2_va1;
+                                                            tr2_va1 = tmp;
+                                                          };
+
+
+
+
+
+
+
+                                                  // Serazeni delek stran tr1:
+
+
+                                                          if ( z > x ){
+                                                            double tmp = x;
+                                                            x = z;
+                                                            z = tmp;
+                                                          };
+
+                                                          if ( x > y ){
+                                                            double tmp = y;
+                                                            y = x;
+                                                            x = tmp;
+                                                          };
+
+                                                          if ( z > x ){
+                                                            double tmp = x;
+                                                            x = z;
+                                                            z = tmp;
+                                                          };
+
+
+                      // Shodnost:
+
+
+                          if ( tr2_va1 == z && tr2_va3 == x && c == y ){
+                            shodne = 1;
+                          }
+                          else{
+                            shodne = 0;
+                          };
+
+                      // Podobnost:
+
+
+                          if ( ( (tr2_va1 / z) == (tr2_va3 / x) ) && ( (tr2_va3 / x) == (c / y) ) && ( (tr2_va1 / z) == (c / y) ) ){
+                            podobne = 1;
+                          }
+                          else{
+                            podobne = 0;
+                          };
+
+
+
+                      // Vypis:
+
+
+                          printf("Triangle 1 = %0.15f %0.15f %0.15f\n", z, x, y);
+                          printf("Triangle 2 = %0.15f %0.15f %0.15f\n", tr2_va1, tr2_va3, c);
+
+
+                          if ( shodne == 1 && podobne == 1 ){
+                            printf("Trojuhelniky jsou shodne.\n");
+                            return 0;
+                          }
+                          else if ( shodne == 1 && podobne == 0 ){
+                            printf("Trojuhelniky jsou shodne.\n");
+                            return 0;
+                          }
+                          else if ( shodne == 0 && podobne == 1 ){
+                            printf("Trojuhelniky nejsou shodne, ale jsou podobne.\n");
+                            return 0;
+                          }
+                          else if ( shodne == 0 && podobne == 0 ){
+                            printf("Trojuhelniky nejsou shodne ani podobne.\n");
+                            return 0;
+                          };
+
+
+
+            }
 
 
   return 0;
