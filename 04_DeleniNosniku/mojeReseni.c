637ce39a056f0fc1e0db71b8039e5e6d6c9df40c
MartinTam <hongvtam@protonmail.com>
2019-11-20 09:51:44 +0100

    04 Deleni nosniku
    algoritmus vymyslen

--- 04_DeleniNosniku/mojeReseni.c
+++ 04_DeleniNosniku/mojeReseni.c
@@ -1,44 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define ARRAY_MAX 500000
 
-int readInput (int * array, int max, int count){
-  int i = 0;
-  int err;
-
-  printf ("Zadejte delky:\n");
-  while ( ( err = scanf("%d", array + i) ) == 1){
-
-    if (array[i] <= 0 || array[i] > max){
-      return 0;
-    };
-    i++;
-    count += count;
-  }
-  if (err == EOF){
-    return 1;
-  }
-  else if (err != EOF){
-    return 0;
-  };
-  return 0;
+int intSortProc ( int * a, int * b )
+{
+  return ( *b < *a ) - ( *a < *b );
 }
 
-int main(){
-  int delky[ARRAY_MAX];
-  int cnt = 0;
-  int i;
 
-  if ( ! readInput(delky, ARRAY_MAX, cnt)){
+int main(){
+  int celkovySoucet = 0;
+  int array[ARRAY_MAX];
+  int i, e;
+  int plus = 0;
+  int err;
+  int pocetPrvku = 0;
+
+
+  while ( ( err = scanf("%d", &array[i++]) ) == 1){
+    pocetPrvku++;
+  };
+  if (err != EOF){
     printf("Nespravny vstup.\n");
     return 1;
   };
 
-  readInput(delky, ARRAY_MAX, cnt);
-  for (i=0; i<cnt; i++){
-    printf("%d ", delky[i]);
+
+  for (i=0; i<11; i++){
+    celkovySoucet += array[i];
   };
 
+  for (e=0; e<9; e++){
+    qsort ( array, 11, sizeof ( *array ), (int(*)(const void *, const void *))intSortProc );
+    plus = array[e] + array[e+1];
+    celkovySoucet += plus;
+    array[e] = 0;
+    array[e+1] = plus;
+  };
 
+  printf("Cena za deleni: %d\n", celkovySoucet);
 
   return 0;
 }
