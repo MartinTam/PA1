995be4731ec6e6792fcb596a08dabc617151bbe9
MartinTam <hongvtam@protonmail.com>
2019-11-20 11:15:38 +0100

    04 - 1.5B

--- 04_DeleniNosniku/mojeReseni.c
+++ 04_DeleniNosniku/mojeReseni.c
@@ -7,10 +7,10 @@ int intSortProc ( int * a, int * b )
   return ( *b < *a ) - ( *a < *b );
 }
 
-
 int main(){
-  int celkovySoucet = 0, i, e, plus = 0, err, pocetPrvku = 0;
+  int i=0, e, plus = 0, err, pocetPrvku = 0;
   int array[ARRAY_MAX];
+  long long int celkovySoucet = 0;
 
   printf("Zadejte delky:\n");
 
@@ -24,9 +24,15 @@ int main(){
 
   pocetPrvku += 1;
 
+  if (pocetPrvku == 1){
+    printf("Nespravny vstup.\n");
+    return 1;
+  };
+
   for (i=0; i<(pocetPrvku - 1); i++){
     if (array[i] <= 0 || array[i] > ARRAY_MAX){
       printf("Nespravny vstup.\n");
+      return 1;
     };
   };
 
@@ -34,20 +40,19 @@ int main(){
     celkovySoucet += array[i];
   };
 
-  if (celkovySoucet == 1){
+  if (pocetPrvku == 2){
     printf("Cena za deleni: 0\n");
+    return 0;
   };
 
   for (e=0; e<(pocetPrvku - 3); e++){
     qsort ( array, pocetPrvku - 1, sizeof ( *array ), (int(*)(const void *, const void *))intSortProc );
     plus = array[e] + array[e+1];
     celkovySoucet += plus;
-    array[e] = 0;
     array[e+1] = plus;
   };
 
-  printf("Cena za deleni: %d\n", celkovySoucet);
-
+  printf("Cena za deleni: %lld\n", celkovySoucet);
 
   return 0;
 }
